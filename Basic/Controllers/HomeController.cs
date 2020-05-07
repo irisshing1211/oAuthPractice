59b7513513fa3dcc0f5fa31a66b80428c192281a
Iris Shing <irisshing1211@gmail.com>
2020-05-07 15:19:19 +0800

    dotnet Authorization

---
+++ Basic/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basic.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index() { return View(); }
+
+        [Authorize]
+        public IActionResult Secret() { return View(); }
+
+        public IActionResult Auth()
+        {
+            // once login -> get all the info needed then
+            var myClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim(ClaimTypes.Email, "bob@mail.com"),
+                new Claim("Welcome", "Hello")
+            };
+
+            var licenseClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "bob K Foo"), new Claim("IdCard", "AA219874")
+            };
+
+            //generate an identity
+            var identity = new ClaimsIdentity(myClaims, "My identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Gov");
+            var userPrincipal = new ClaimsPrincipal(new[] {identity, licenseIdentity});
+            // 
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
