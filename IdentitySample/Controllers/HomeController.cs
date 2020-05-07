79c97c5aac1155f0045b2fb01b9df76a71b9d375
Iris Shing <irisshing1211@gmail.com>
2020-05-07 16:10:46 +0800

    Identity Authentication

---
+++ IdentitySample/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentitySample.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentitySample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser> _signInManager;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // GET
+        public IActionResult Index() { return View(); }
+
+        [Authorize]
+        public IActionResult Secret() { return View(); }
+
+        public IActionResult Login() { return View(); }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            // check user exist
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                // sign in
+                var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+                if (signInResult.Succeeded) { return RedirectToAction("Index"); }
+            }
+
+            // can access
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Reg() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Reg(string username, string password)
+        {
+            var user = new IdentityUser {UserName = username, Email = ""};
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                // sign user here
+                var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+                if (signInResult.Succeeded) { return RedirectToAction("Index"); }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ IdentitySample/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentitySample.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentitySample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(config => { config.UseInMemoryDatabase("Memory"); });
+
+            services.AddIdentity<IdentityUser, IdentityRole>(config =>
+                    {
+                        // set password rule
+                        config.Password.RequiredLength = 4;
+                        config.Password.RequireDigit = false;
+                        config.Password.RequireNonAlphanumeric = false;
+                        config.Password.RequireUppercase = false;
+                       
+                    })
+                    .AddEntityFrameworkStores<AppDbContext>()
+                    .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "TestCookie";
+                config.LoginPath = "/Home/Login";
+            });
+            // services.AddAuthentication("CookieAuth")
+            //         .AddCookie("CookieAuth",
+            //                    config =>
+            //                    {
+            //                        config.Cookie.Name = "MyCookie";
+            //
+            //                        // if no auth -> redirect to login path
+            //                        config.LoginPath = "/Home/Auth";
+            //                    });
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+
+            app.UseRouting();
+
+            // who r u
+            app.UseAuthentication();
+
+            // ask if the request is allow to the page
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+        }
+    }
+}
