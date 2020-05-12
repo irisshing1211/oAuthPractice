73d9f85a07d070f950964a0c21b53e1bbe25a6a0
Iris Shing <irisshing1211@gmail.com>
2020-05-12 17:15:34 +0800

    get the claim from token in client

--- Client/Startup.cs
+++ Client/Startup.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace Client
 {
@@ -35,22 +39,41 @@ namespace Client
                                   config.ClientId = "client_id";
                                   config.ClientSecret = "client_secret";
                                   config.CallbackPath = "/oauth/callback";
+
                                   // server endpoint
                                   config.AuthorizationEndpoint = "https://localhost:44345/oauth/auth";
                                   config.TokenEndpoint = "https://localhost:44345/oauth/token";
                                   config.SaveTokens = true; // told the client to save the token so that can use it
+
+                                  config.Events = new OAuthEvents
+                                  {
+                                      OnCreatingTicket = context =>
+                                      {
+                                          var accessToken = context.AccessToken;
+                                          var base64payload = accessToken.Split('.')[1];
+                                          var bytes = Convert.FromBase64String(base64payload);
+                                          var jsonPayload = Encoding.UTF8.GetString(bytes);
+
+                                          var claims =
+                                              JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonPayload);
+
+                                          foreach (var claim in claims)
+                                          {
+                                              context.Identity.AddClaim(new Claim(claim.Key, claim.Value));
+                                          }
+
+                                          return Task.CompletedTask;
+                                      }
+                                  };
                               });
 
-        services.AddControllersWithViews();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
 
             app.UseRouting();
 
